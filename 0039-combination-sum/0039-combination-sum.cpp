4bf43afbc4e635faa26d188ab368d8d9ce7567de
Abdul Mannan <mannan.jnv123@gmail.com>
2024-05-21 15:54:49 +0530

    Time: 8 ms (45.76%), Space: 12.2 MB (85.40%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void subsetsum(int ind,int n,vector<int>&d,vector<vector<int>>&ans,vector<int>&candidates,int target,int sum){
+        if(ind==n){
+           if(sum==target){
+            ans.push_back(d);
+        }
+            return ;
+        }
+        
+        if(sum<=target){
+     d.push_back(candidates[ind]);
+     subsetsum(ind,n,d,ans,candidates,target,sum+candidates[ind]);
+        d.pop_back();
+      }
+        subsetsum(ind+1,n,d,ans,candidates,target,sum);
+    }
+    
+    
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>d;
+        int n=candidates.size();
+        int sum=0;
+        subsetsum(0,n,d,ans,candidates,target,sum);
+        return ans;
+    }
+};
