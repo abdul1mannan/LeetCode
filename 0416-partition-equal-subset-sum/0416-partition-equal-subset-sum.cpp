a82b379523e2322e9bb1148c9bb9b40fc39edfb5
Abdul Mannan <mannan.jnv123@gmail.com>
2024-07-01 17:41:20 +0530

    Time: 299 ms (22.95%), Space: 16.2 MB (68.93%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    bool subsetsum(vector<int>& nums, int target, int index, vector<vector<int>>& memo) {
-        int n = nums.size();
-        if (target == 0) return true;
-        if (index == n || target < 0) return false;
-        
-        // Check memoization
-        if (memo[index][target] != -1) return memo[index][target];
-        
-        // Exclude the current number
-        bool exclude = subsetsum(nums, target, index + 1, memo);
-        
-        // Include the current number (only if it does not exceed the target)
-        bool include = false;
-        if (nums[index] <= target) {
-            include = subsetsum(nums, target - nums[index], index + 1, memo);
-        }
-        
-        // Store the result in memo
-        return memo[index][target] = include || exclude;
-    }
-    
     bool canPartition(vector<int>& nums) {
-        int sum = 0;
+         int sum = 0;
+        int n=nums.size();
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
         }
         if (sum % 2 != 0) return false;
         
         int target = sum / 2;
-        vector<vector<int>> memo(nums.size(), vector<int>(target + 1, -1));
-        return subsetsum(nums, target, 0, memo);
+        
+           vector<vector<bool>> dp(nums.size()+1, vector<bool>(target + 1, 0));
+           for (int i = 0; i <= n; i++) {
+              dp[i][0] = true;
+       }
+        
+      for (int i = 1; i <= target; i++) {
+        dp[0][i] = false;
+      }
+        for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= target; j++) {
+           
+            if (nums[i - 1] > j) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+              
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+            }
+        }
+    }
+    return dp[n][target];
+        
     }
 };
