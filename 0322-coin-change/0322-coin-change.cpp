4b63d16e0453c678ca964f42d1f08b0d2659623d
Abdul Mannan <mannan.jnv123@gmail.com>
2024-07-02 19:24:55 +0530

    Time: 44 ms (38.42%), Space: 51 MB (13.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    long long int mincoin(vector<int> &coins,int amount,vector<vector<int>> &dp,int index)
-    {
-        int n=coins.size();
-        if(amount<0)
-            return INT_MAX;
-        if(index==n && amount!=0)
-        return INT_MAX;
-        if(amount==0)
-            return 0;
-        if(dp[index][amount]!=-1)
-            return dp[index][amount];
-        
-        long long int exclude=mincoin(coins,amount,dp,index+1);
-        long long int include=INT_MAX;
-        if(coins[index]<=amount){
-           include=1+mincoin(coins,amount-coins[index],dp,index);
-   
-        }
-        return dp[index][amount]=min(exclude,include);
-        
-        
-    }
+  
     int coinChange(vector<int>& coins, int amount) { 
-        int n=coins.size();
-        vector<vector<int>> dp(n+1,vector<int> (amount+1,-1));
-     
-         int result = mincoin(coins, amount, dp, 0);
-        return result == INT_MAX ? -1 : result;
+       int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX));
+
+        
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] <= j) {
+                    if (dp[i][j - coins[i - 1]] != INT_MAX) {
+                        dp[i][j] = min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+                    } else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][amount] == INT_MAX ? -1 : dp[n][amount];
         
     }
 };
