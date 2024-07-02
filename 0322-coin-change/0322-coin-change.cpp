685058022450898e03d821dbab76818299c61b73
Abdul Mannan <mannan.jnv123@gmail.com>
2024-07-02 19:15:27 +0530

    Time: 69 ms (10.54%), Space: 52 MB (8.46%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int mincoin(vector<int> &coins,int amount,vector<vector<int>> &dp,int index)
+    long long int mincoin(vector<int> &coins,int amount,vector<vector<int>> &dp,int index)
     {
         int n=coins.size();
         if(amount<0)
@@ -8,17 +8,15 @@ public:
         if(index==n && amount!=0)
         return INT_MAX;
         if(amount==0)
-            return 0;//abdul
+            return 0;
         if(dp[index][amount]!=-1)
             return dp[index][amount];
         
-        int exclude=mincoin(coins,amount,dp,index+1);
-        int include=INT_MAX;
+        long long int exclude=mincoin(coins,amount,dp,index+1);
+        long long int include=INT_MAX;
         if(coins[index]<=amount){
-           int res=mincoin(coins,amount-coins[index],dp,index);
-        if(res!=INT_MAX){
-            include=res+1;
-        }
+           include=1+mincoin(coins,amount-coins[index],dp,index);
+   
         }
         return dp[index][amount]=min(exclude,include);
         
@@ -27,7 +25,7 @@ public:
     int coinChange(vector<int>& coins, int amount) { 
         int n=coins.size();
         vector<vector<int>> dp(n+1,vector<int> (amount+1,-1));
-        sort(coins.begin(),coins.end(),greater<int>());
+     
          int result = mincoin(coins, amount, dp, 0);
         return result == INT_MAX ? -1 : result;
         
