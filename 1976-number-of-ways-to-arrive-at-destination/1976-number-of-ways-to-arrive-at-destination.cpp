f2a151f5e7e40e83a580501bb8c4625829ba1202
Abdul Mannan <mannan.jnv123@gmail.com>
2024-07-26 01:21:59 +0530

    Time: 50 ms (86.84%), Space: 37.3 MB (32.22%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
-    
-    long long mod = (long long)1e9+7;
+     long long mod = (long long)1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        
-        vector <vector<pair<long long,long long>>> adj(n);
-        for(int i=0 ; i<roads.size() ; i++){
-            int u = roads[i][0];
-            int v = roads[i][1];
-            int w = roads[i][2];
-            adj[u].push_back({v,w});
-            adj[v].push_back({u,w});
+    
+        vector<vector<pair<long long, long long>>> adj(n);
+
+        // Building adjacency list
+        for (auto& it : roads) {
+            adj[it[0]].emplace_back(it[1], it[2]);
+            adj[it[1]].emplace_back(it[0], it[2]);
         }
 
-        // Time , Node 
-        priority_queue <pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>> pq;
-        vector <long long> time(n,LLONG_MAX);
-        vector <long long> ways(n,0);
+        vector<long long> dist(n,LLONG_MAX);
+        vector<long long> ways(n, 0);
+        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>,          greater<pair<long long, long long>>> q;
 
-        pq.push({0,0});
-        time[0] = 0;
+        // Starting from node 0
+        dist[0] = 0;
         ways[0] = 1;
+        q.push({0, 0});
 
-        while(!pq.empty()){
+        while (!q.empty()) {
+            long long int d = q.top().first;
+            long long node = q.top().second;
+            q.pop();
 
-            long long t = pq.top().first;
-            long long node = pq.top().second;
-            pq.pop();
+            for (auto edge : adj[node]) {
+                long long int adjNode = edge.first;
+                long long cost = edge.second;
 
-            for(auto it : adj[node]){
-                long long v = it.first;
-                long long wt = it.second;
-                if(t + wt < time[v]){
-                    time[v] = t + wt;
-                    ways[v] = ways[node];
-                    pq.push({time[v],v});
-                }
-                else if(t + wt == time[v] ){
-                    ways[v] = (ways[v] + ways[node])%mod;
+                if (cost + d < dist[adjNode]) {
+                    dist[adjNode] = cost + d;
+                   
+                    ways[adjNode] = ways[node];
+                     q.push({dist[adjNode], adjNode});
+                } else if (cost + d == dist[adjNode]) {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
                 }
             }
         }
-        return ways[n-1]%mod;
+
+        return (ways[n - 1] % mod);
     }
 };
