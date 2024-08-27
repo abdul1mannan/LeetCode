53a09b4695c24f846b39cfe723e2918b84bed80b
Abdul Mannan <mannan.jnv123@gmail.com>
2024-08-27 13:49:18 +0530

    Time: 13 ms (7.77%), Space: 17.3 MB (34.89%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n=image.size();
+        int m=image[0].size();
         
+        int dx[]={1,-1,0,0};
+        int dy[]={0,0,1,-1};
+        
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
         int initial=image[sr][sc];
         if(initial==color){
             return image;
         }
-          int n=image.size();
-        int m=image[0].size();
         image[sr][sc]=color;
-     
-        queue<pair<int,int>>q;
-        int dx[4]={1,-1,0,0};
-        int dy[4]={0,0,1,-1};
-        q.push({sr,sc});
         while(!q.empty()){
-    
             int x=q.front().first;
             int y=q.front().second;
+    
             q.pop();
-          
             for(int i=0;i<4;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-                if(nx>=0 &&ny>=0 &&nx<n && ny<m && image[nx][ny]==initial){
+                int nx=dx[i]+x;
+                int ny=dy[i]+y;
+                
+                if(nx>=0 && ny>=0 && nx<n && ny<m && image[nx][ny]==initial){
                     q.push({nx,ny});
                     image[nx][ny]=color;
-                  
                 }
             }
-          
         }
         return image;
-    
     }
 };
