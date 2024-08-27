f43d5985577a3f6fc2fd9f3b1f31ab26529604f1
Abdul Mannan <mannan.jnv123@gmail.com>
2024-08-27 13:38:48 +0530

    Time: 10 ms (18.11%), Space: 16.2 MB (63.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,63 +3,54 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-     
-        
-        int rotten=0;
         int total=0;
+        int rotten=0;
         queue<pair<int,int>>q;
-        int dx[4]={1,-1,0,0};
-        int dy[4]={0,0,1,-1};
-            for(int i=0;i<n;i++){
-                for(int j=0;j<m;j++){
-                    if(grid[i][j]==2){
-                    
-                        total++;
-                        q.push({i,j});
-                    }
-                    if(grid[i][j]==1){
-                        total++;
-                    }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                    rotten++;
+                    total++;
+                }
+                if(grid[i][j]==1){
+                    total++;
                 }
             }
-        int cnt=0;
+        }
+        
+        int dx[]={1,-1,0,0};
+        int dy[]={0,0,-1,1};
+        int min=0;
         while(!q.empty()){
-            rotten=q.size();
-            
-             while(rotten--){
+            int s=q.size();
+           
+            while(s--){
+                
             int x=q.front().first;
             int y=q.front().second;
             q.pop();
-          
-          
+         
+        
             for(int i=0;i<4;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-                if(nx>=0 &&ny>=0 &&nx<n && ny<m && grid[nx][ny]==1){
+                int nx=dx[i]+x;
+                int ny=dy[i]+y;
+                
+                if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==1){
                     q.push({nx,ny});
                     grid[nx][ny]=2;
+                    rotten++;
                 }
             }
-                
-                
-                
-                
-                }
-             if(!q.empty()){
-                     cnt++;
-                 }
+            }
+           if(!q.empty()){
+               min++;
+           }
+            
         }
-        int t=0;
-           for(int i=0;i<n;i++){
-                for(int j=0;j<m;j++){
-                  if(grid[i][j]==2){
-                t++;
-                        
-                    }}}
-        if(t==total)
-               return cnt;
-        
+        if(rotten==total){
+            return min;
+        }
         else return -1;
-        
     }
 };
