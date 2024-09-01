594028afda1ed58eff1f5ec9f999935f59258b43
Abdul Mannan <mannan.jnv123@gmail.com>
2024-09-01 17:06:07 +0530

    Time: 45 ms (65.01%), Space: 24.2 MB (66.23%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -11,28 +11,28 @@ public:
         }
         int dx[]={1,-1,0,0,1,-1,1,-1};
         int dy[]={0,0,1,-1,1,-1,-1,1};
-        queue<pair<pair<int,int>,int>>q;
-        q.push({{0,0},1});
+        queue<pair<int,int>>q;
+        q.push({0,0});
         
         vector<vector<int>>dis(n,vector<int>(n,1e9));
         dis[0][0]=1;
         while(!q.empty()){
             
             auto it=q.front();
-            int x=it.first.first;
-            int y=it.first.second;
-            int dist=it.second;
+            int x=it.first;
+            int y=it.second;
+        
             q.pop();
           
             for(int i=0;i<8;i++){
                 int nx=x+dx[i];
                 int ny=y+dy[i];
-                if(nx>=0 && ny>=0 && nx<n && ny<n && grid[nx][ny]==0 && dis[nx][ny]>dist+1){
-                    dis[nx][ny]=dist+1;
+                if(nx>=0 && ny>=0 && nx<n && ny<n && grid[nx][ny]==0 && dis[nx][ny]>dis[x][y]+1){
+                    dis[nx][ny]=dis[x][y]+1;
                       if(nx==n-1 && ny==n-1){
-                return dist+1;
+                return dis[nx][ny];
             }
-                    q.push({{nx,ny},dist+1});
+                    q.push({nx,ny});
                 }
             }
         }
