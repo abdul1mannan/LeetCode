4f94be4d19c17ad44119e164759d77753bd26a82
Abdul Mannan <mannan.jnv123@gmail.com>
2024-09-02 13:01:08 +0530

    Time: 98 ms (19.07%), Space: 47.5 MB (26.26%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-           vector<vector<pair<long long, long long>>> adj(n+1);
-
-    
-        for (auto& it : times) {
-            adj[it[0]].emplace_back(it[1], it[2]);
-
+        
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(auto it:times){
+            adj[it[0]].push_back({it[1],it[2]});
         }
-          vector<long long> dist(n+1,LLONG_MAX);
-      
-        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>,          greater<pair<long long, long long>>> q;
-
-      
-        dist[k] = 0;
-      
-        q.push({0, k});
-
-        while (!q.empty()) {
-            long long int d = q.top().first;
-            long long node = q.top().second;
+        
+        vector<int>dis(n+1,1e9);
+        dis[k]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        q.push({0,k});
+        
+        while(!q.empty()){
+            int t=q.top().second;
+            int dist=q.top().first;
             q.pop();
-
-            for (auto edge : adj[node]) {
-                long long int adjNode = edge.first;
-                long long cost = edge.second;
-
-                if (cost + d < dist[adjNode]) {
-                    dist[adjNode] = cost + d;
-                   q.push({dist[adjNode], adjNode});
-                } 
+            for(auto &it:adj[t]){
+                int node=it.first;
+                int time=it.second;
+                if((time+dist)<dis[node]){
+                    dis[node]=time+dist;
+                    q.push({time+dist,node});
+                }
             }
         }
-        long long int time=LLONG_MIN;
-
+        int mini=0;
         for(int i=1;i<=n;i++){
-            if(dist[i]==LLONG_MAX){
+            if(dis[i]==1e9){
                 return -1;
             }
-            time=max(time,dist[i]);
+            mini=max(mini,dis[i]);
         }
-      
-    return time;
-
+        return mini;
     }
 };
