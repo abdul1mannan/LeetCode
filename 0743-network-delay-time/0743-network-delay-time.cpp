db50c26e4b7ef5556e4e0ed13a0ee49dceaf2351
Abdul Mannan <mannan.jnv123@gmail.com>
2024-09-02 13:02:48 +0530

    Time: 74 ms (83.02%), Space: 43.9 MB (70.28%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        
-        vector<vector<pair<int,int>>>adj(n+1);
-        for(auto it:times){
-            adj[it[0]].push_back({it[1],it[2]});
-        }
-        
-        vector<int>dis(n+1,1e9);
-        dis[k]=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
-        q.push({0,k});
-        
-        while(!q.empty()){
-            int t=q.top().second;
-            int dist=q.top().first;
-            q.pop();
-            for(auto &it:adj[t]){
-                int node=it.first;
-                int time=it.second;
-                if((time+dist)<dis[node]){
-                    dis[node]=time+dist;
-                    q.push({time+dist,node});
-                }
-            }
-        }
-        int mini=0;
-        for(int i=1;i<=n;i++){
-            if(dis[i]==1e9){
-                return -1;
-            }
-            mini=max(mini,dis[i]);
-        }
-        return mini;
+      vector<vector<pair<int, int>>> adj(n + 1);
+
+ 
+    for (auto& it : times) {
+        adj[it[0]].push_back({it[1], it[2]});
     }
+
+    vector<int> dis(n + 1, 1e9);
+    dis[k] = 0; 
+
+
+    queue<int> q;
+    q.push(k);
+
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        for (auto& it : adj[node]) {
+            int nextNode = it.first;
+            int time = it.second;
+            if (dis[node] + time < dis[nextNode]) {
+                dis[nextNode] = dis[node] + time;
+                q.push(nextNode); 
+            }
+        }
+    }
+
+
+    int maxTime = 0;
+    for (int i = 1; i <= n; i++) {
+        if (dis[i] == 1e9) {
+            return -1; 
+        }
+        maxTime = max(maxTime, dis[i]);
+    }
+
+    return maxTime;
+}
 };
