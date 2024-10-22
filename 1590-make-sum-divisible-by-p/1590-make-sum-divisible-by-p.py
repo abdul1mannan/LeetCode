42cd8b295756fbf583eddc3aed90db7452c273d6
Abdul Mannan <mannan.jnv123@gmail.com>
2024-10-22 18:49:04 +0530

    Time: 88 ms (99.45%), Space: 37.1 MB (7.20%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        n = len(nums)
+        
+        totalSum=0
+        
+        for num in nums:
+            totalSum = (totalSum+num) % p
+            
+        if totalSum==0:
+            return 0
+        
+        prefixModMap = {}
+        
+        prefixModMap[0]=-1
+        
+        prefixSum = 0
+        
+        minLen = n
+        
+        for i in range(0,n):
+            prefixSum = (prefixSum+nums[i])%p
+            target = (prefixSum-totalSum+p)%p
+            
+            if target in prefixModMap:
+                minLen=min(minLen,i-prefixModMap[target])
+                
+            prefixModMap[prefixSum]=i
+            
+        if minLen == n:
+            return -1
+        else:
+            return minLen
+  
