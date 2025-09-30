34b156b0baf7718d8755dd68e1963fa68b1c2c13
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:20:03 -0400

    Create 131-PalindromePartitioning

---
+++ 131-PalindromePartitioning/131-PalindromePartitioning.cpp
@@ -0,0 +1,35 @@
+// Last updated: 10/1/2025, 12:49:58 AM
+class Solution {
+public:
+    bool isPalindrome(string s){
+        string t=s;
+        reverse(t.begin(),t.end());
+        if(t==s){
+            return true;
+        }
+        else return false;
+    }
+    void partitionString(int ind,int n,string &s,vector<vector<string>>&ans,vector<string>&d){
+        
+        if(ind==n){
+            ans.push_back(d);
+            return ;
+        }
+        for(int i=ind;i<n;i++){
+          string sub=s.substr(ind,i-ind+1);
+         if(isPalindrome(sub)){
+         d.push_back(sub);
+         partitionString(i+1,n,s,ans,d);
+         d.pop_back();
+        }
+        
+           }
+           }
+    vector<vector<string>> partition(string s) {
+        int n=s.size();
+        vector<vector<string>>ans;
+        vector<string>d;
+        partitionString(0,n,s,ans,d);
+        return ans;
+    }
+};
