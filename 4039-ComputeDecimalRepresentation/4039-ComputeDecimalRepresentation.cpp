408efed7524472d2619690617fb0d2224aab84fa
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:16:48 -0400

    Create 4039-ComputeDecimalRepresentation

---
+++ 4039-ComputeDecimalRepresentation/4039-ComputeDecimalRepresentation.cpp
@@ -0,0 +1,20 @@
+// Last updated: 10/1/2025, 12:46:43 AM
+class Solution {
+public:
+    vector<int> decimalRepresentation(int n) {
+        vector<int> ans;
+        long long cnt = 1;
+        while (n > 0) {
+            int digit = n % 10;
+            long long val = digit * cnt;
+            cnt = cnt * 10;
+            n = n / 10;
+            if (digit == 0)
+                continue;
+            ans.push_back(val);
+            
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
