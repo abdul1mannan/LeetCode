89384e15df176a4be05995196013cc28a6c4a6ec
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:18:13 -0400

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.cpp
@@ -0,0 +1,33 @@
+// Last updated: 10/1/2025, 12:48:09 AM
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+
+        for (int a : asteroids) {
+            bool destroyed = false;
+            while (!st.empty() && a < 0 && st.top() > 0) {
+                if (st.top() < -a) {
+                    st.pop();       
+                    continue;
+                } else if (st.top() == -a) {
+                    st.pop();       
+                }
+                destroyed = true;  
+                break;
+            }
+
+            if (!destroyed) {
+                st.push(a);
+            }
+        }
+
+       
+        vector<int> res(st.size());
+        for (int i = st.size() - 1; i >= 0; i--) {
+            res[i] = st.top();
+            st.pop();
+        }
+        return res;
+    }
+};
