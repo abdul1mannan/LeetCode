84bfd42060a274905de8265d7da10e7139ba3c16
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:18:22 -0400

    Create 673-NumberOfLongestIncreasingSubsequence

---
+++ 673-NumberOfLongestIncreasingSubsequence/673-NumberOfLongestIncreasingSubsequence.cpp
@@ -0,0 +1,35 @@
+// Last updated: 10/1/2025, 12:48:18 AM
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+          int n = nums.size();
+    if (n == 0) return 0;
+
+    vector<int> dp(n, 1);
+    vector<int> counts(n, 1);  
+
+    int maxLength = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (nums[i] > nums[j]) {
+                if (dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    counts[i] = counts[j]; 
+                } else if (dp[i] == dp[j] + 1) {
+                    counts[i] += counts[j]; 
+                }
+            }
+        }
+        maxLength = max(maxLength, dp[i]);
+    }
+
+    int numberOfLIS = 0;
+    for (int i = 0; i < n; ++i) {
+        if (dp[i] == maxLength) {
+            numberOfLIS += counts[i];
+        }
+    }
+        return numberOfLIS;
+    }
+};
