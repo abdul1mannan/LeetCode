591c3d3adaa9a4d98d298055fdf7e1d23b37a0bd
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:20:49 -0400

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -0,0 +1,13 @@
+// Last updated: 10/1/2025, 12:50:44 AM
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int steps=m+n-2;
+        int r=m-1;
+        long long int res=1;
+        for(int i=1;i<=r;i++){
+            res=res*(steps-r+i)/i;
+        }
+        return res;
+    }
+};
