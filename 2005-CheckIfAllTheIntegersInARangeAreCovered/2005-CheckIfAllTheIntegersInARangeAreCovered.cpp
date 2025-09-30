883e3889914854459f7794fcc6ce472322948af5
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:17:14 -0400

    Create 2005-CheckIfAllTheIntegersInARangeAreCovered

---
+++ 2005-CheckIfAllTheIntegersInARangeAreCovered/2005-CheckIfAllTheIntegersInARangeAreCovered.cpp
@@ -0,0 +1,20 @@
+// Last updated: 10/1/2025, 12:47:09 AM
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        map<int,int>cream;
+        for(auto &i:ranges){
+            cream[i[0]]++;
+            cream[i[1]+1]--;
+            
+        }
+        int count=0;
+        for(int i=0;i<51;i++){
+            count+=cream[i];
+            if(i>=left&& i<=right&&count<=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
