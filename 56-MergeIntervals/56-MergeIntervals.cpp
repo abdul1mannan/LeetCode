60cee984587a735688ccc640ec282d4792e177ab
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:20:53 -0400

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.cpp
@@ -0,0 +1,23 @@
+// Last updated: 10/1/2025, 12:50:48 AM
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+      
+        ans.push_back(intervals[0]);
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=ans.back()[1]){
+               ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            }
+            else {
+               ans.push_back(intervals[i]);
+            }
+          
+        }
+  
+        return ans;
+        
+    }
+};
