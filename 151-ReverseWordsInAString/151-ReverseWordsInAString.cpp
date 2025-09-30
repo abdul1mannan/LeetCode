aac87ab893a09a147c47a4fcd1d7e1c84a33752b
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:19:47 -0400

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.cpp
@@ -0,0 +1,30 @@
+// Last updated: 10/1/2025, 12:49:43 AM
+class Solution {
+public:
+    string reverseWords(string s) {
+      int n=s.size();
+        string temp="";
+        string str="";
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            char ch=s[left];
+            if(ch!=' '){
+                temp+=ch;
+               
+            }
+            else if(temp!=""&&ch==' '){
+                if(str!=""){ str=temp+' '+str;}
+                else str=temp;
+                temp="";
+            }
+             left++;
+        }
+        if(temp!=""){
+           if(str!="") str=temp+' '+str;
+            else str=temp;
+        }
+     
+        return str;
+    }
+};
