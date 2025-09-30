7cb992cfaafc7072d7c4f1f7a66b40974a768c5f
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:21:37 -0400

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,21 @@
+// Last updated: 10/1/2025, 12:51:32 AM
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> hash(256, -1);  
+        int l = 0;
+        int maxi = 0;
+
+        for (int r = 0; r < (int)s.size(); r++) {
+            char c = s[r];
+
+            if (hash[c] != -1 && hash[c] >= l) {
+                l = hash[c] + 1;  
+            }
+
+            hash[c] = r; 
+            maxi = max(maxi, r - l + 1);
+        }
+        return maxi;
+    }
+};
