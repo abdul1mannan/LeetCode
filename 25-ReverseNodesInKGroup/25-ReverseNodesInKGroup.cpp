e6952432c9d89605321cc69078c0b43dc7a6bf5d
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:21:18 -0400

    Create 25-ReverseNodesInKGroup

---
+++ 25-ReverseNodesInKGroup/25-ReverseNodesInKGroup.cpp
@@ -0,0 +1,63 @@
+// Last updated: 10/1/2025, 12:51:14 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *reversell(ListNode *head){
+        
+        if(head==NULL ||head->next==NULL){
+            return head;
+        }
+        
+        ListNode*newhead=reversell(head->next);
+        ListNode *front=head->next;
+        front->next=head;
+        head->next=NULL;
+        
+        return newhead;
+    }
+    
+    ListNode *findkth(ListNode *head,int k){
+        
+        k--;
+        while(head!=NULL && k>0){
+            k--;
+            head=head->next;
+        }
+        return head;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode *temp=head;
+        ListNode *prev=NULL;
+        while(temp!=NULL){
+        ListNode *kthnode=findkth(temp,k);
+        if(kthnode==NULL){
+            if(prev){
+                prev->next=temp;
+            }
+            break;
+        }
+            ListNode *coming=kthnode->next;
+            kthnode->next=NULL;
+             reversell(temp);
+            if(temp==head){
+                head=kthnode;
+            }
+            else {
+                prev->next=kthnode;
+        }
+        prev=temp;
+        temp=coming;
+        }
+        return head;
+    }
+};
