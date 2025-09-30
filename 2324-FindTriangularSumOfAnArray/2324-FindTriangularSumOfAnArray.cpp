6c6d28a99e7528881fee128bb4c8022ed87d0452
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:17:04 -0400

    Create 2324-FindTriangularSumOfAnArray

---
+++ 2324-FindTriangularSumOfAnArray/2324-FindTriangularSumOfAnArray.cpp
@@ -0,0 +1,13 @@
+// Last updated: 10/1/2025, 12:46:59 AM
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int n = nums.size();
+        for (int size = n; size > 1; --size) {
+            for (int i = 0; i < size - 1; ++i) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+        }
+        return nums[0];
+    }
+};
