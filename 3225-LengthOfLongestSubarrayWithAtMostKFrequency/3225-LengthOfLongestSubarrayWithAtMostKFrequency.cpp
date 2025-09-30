cfed9cfbcb31933950607410b4a7d81ef2b43e00
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:17:00 -0400

    Create 3225-LengthOfLongestSubarrayWithAtMostKFrequency

---
+++ 3225-LengthOfLongestSubarrayWithAtMostKFrequency/3225-LengthOfLongestSubarrayWithAtMostKFrequency.cpp
@@ -0,0 +1,27 @@
+// Last updated: 10/1/2025, 12:46:55 AM
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int left=0;
+        map<int,int>freq;
+        int n=nums.size();
+        int count=0;
+        int maxi=0;
+        int j=0;
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+            if(freq[nums[i]]==k+1){
+                while(freq[nums[i]]==k+1){
+                    --freq[nums[j]];
+                    ++j;
+                }
+               count=i-j+1;
+                
+            }
+            else count++;
+            
+           maxi=max(maxi,count);
+        }
+        return maxi;
+    }
+};
