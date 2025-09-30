520e49f455aeed81142e0a10cce584db16824d5c
Abdul Mannan <mannan.jnv123@gmail.com>
2025-09-30 15:18:53 -0400

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.cpp
@@ -0,0 +1,11 @@
+// Last updated: 10/1/2025, 12:48:49 AM
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+};
