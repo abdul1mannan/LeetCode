6ca796f3ecf1db78a3649253ac9f96fb38790f19
Abdul Mannan <mannan.jnv123@gmail.com>
2025-10-02 12:21:47 -0400

    Time: 55 ms (71.5%), Space: 21.1 MB (76.63%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        
-        int count=0;
-        
-        queue<pair<string,int>>q;
-        q.push({beginWord,1});
-        
-        unordered_set<string>st(wordList.begin(),wordList.end());
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
+        unordered_set<string> st(wordList.begin(), wordList.end());
         st.erase(beginWord);
-        if(st.find(endWord)==st.end()){
-            return 0;
-        }
-        
-        while(!q.empty()){
-            string s=q.front().first;
-            int step=q.front().second;
-            q.pop();
-            if(s==endWord){
-                return step;
-            }
-            for(int i=0;i<s.size();i++){
-                char orig=s[i];
-                
-                for(char j='a';j<='z';j++){
-                    s[i]=j;
-                    if(st.find(s)!=st.end()){
-                        q.push({s,step+1});
-                        st.erase(s);
-                    }
-                    
 
+        while (!q.empty()) {
+            string word = q.front().first;
+            int dis = q.front().second;
+            q.pop();
+            if (word == endWord) {
+                return dis;
+            }
+
+            for (int i = 0; i < word.size(); i++) {
+
+                char org = word[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    word[i] = ch;
+                    if (st.find(word) != st.end()) {
+                        st.erase(word);
+                        q.push({word,dis+1});
+                    }
                 }
-                s[i]=orig;
+                word[i]=org;
             }
         }
         return 0;
