3479dd0a2975b1fd419ba05742c80b316757a702
Abdul Mannan <mannan.jnv123@gmail.com>
2025-10-02 08:05:23 -0400

    Time: 0 ms (100%), Space: 18.3 MB (46.72%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,32 +3,33 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
         int m=image[0].size();
-        
-        int dx[]={1,-1,0,0};
-        int dy[]={0,0,1,-1};
-        
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        int initial=image[sr][sc];
-        if(initial==color){
+        if(image[sr][sc]==color){
             return image;
         }
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        int col=image[sr][sc];
         image[sr][sc]=color;
+        int dx[]={1,-1,0,0};
+        int dy[]={0,0,1,-1};
         while(!q.empty()){
-            int x=q.front().first;
-            int y=q.front().second;
-    
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nx=dx[i]+x;
-                int ny=dy[i]+y;
-                
-                if(nx>=0 && ny>=0 && nx<n && ny<m && image[nx][ny]==initial){
-                    q.push({nx,ny});
-                    image[nx][ny]=color;
+            int s=q.size();
+            while(s--){
+                int x=q.front().first;
+                int y=q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int nx=x+dx[i];
+                    int ny=y+dy[i];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && image[nx][ny]==col){
+                     image[nx][ny]=color;
+                     q.push({nx,ny});
+                    }
+                    
                 }
             }
         }
         return image;
+   
     }
 };
