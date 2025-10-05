b4eeac26db1db3e232d00dba39ac75aaa37074ba
Abdul Mannan <mannan.jnv123@gmail.com>
2025-10-05 06:16:16 -0400

    Time: 0 ms (100%), Space: 8.6 MB (22.73%) - LeetHub

--- 2939-maximum-xor-product/2939-maximum-xor-product.cpp
+++ 2939-maximum-xor-product/2939-maximum-xor-product.cpp
@@ -2,31 +2,14 @@ class Solution {
 public:
     int maximumXorProduct(long long a, long long b, int n) {
       
-        int mod = 1e9 + 7;
-        if(n==0) return (a%mod)*(b%mod)%mod;
-        long long x = 0;
-        long long max_val = 0;
-
-        for (long long i = n - 1; i >= 0; i--) {
-            long long mask = 1LL << i;
-
-            bool bit_a = (a & mask) > 0;
-            bool bit_b = (b & mask) > 0;
-            long long ax = a ^ x;
-            long long bx = b ^ x;
-            if (bit_a == bit_b) {
-              if (bit_a == 0) {
-                x |= mask;
+        int MOD = 1e9 + 7;
+        if(n==0) return (a%MOD)*(b%MOD)%MOD;
+        while(n--){
+            if((min(a,b)&(1LL<<n))==0){
+                a^=1LL<<n;
+                b^=1LL<<n;
             }
-            } else {
-                if((min(ax,bx)&(mask))==0){
-                    x|=mask;
-                }
-            }
-            ax=a^x;
-            bx=b^x;
-            max_val=(ax%mod) * (bx%mod)%mod;
         }
-        return max_val;
+         return (a % MOD) * (b % MOD) % MOD;
     }
 };
