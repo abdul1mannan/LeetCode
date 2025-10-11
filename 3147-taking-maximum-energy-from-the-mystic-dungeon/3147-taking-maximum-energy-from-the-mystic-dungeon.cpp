d7d42cda4a459f861dc34e70267a30c31d07b30a
Abdul Mannan <mannan.jnv123@gmail.com>
2025-10-11 04:43:32 -0400

    Time: 160 ms (10.89%), Space: 158 MB (10.23%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,13 +1,31 @@
 class Solution {
+    int n, K;
+    vector<int> memo;
+
+    int dfs(int i, vector<int>& energy) {
+        if (i >= n)
+            return 0;
+        if (memo[i] != INT_MIN)
+            return memo[i];
+
+        int best = energy[i];
+        if (i + K < n) {
+            best += dfs(i + K, energy);
+        }
+
+        return memo[i] = best;
+    }
+
 public:
     int maximumEnergy(vector<int>& energy, int k) {
-        int n = energy.size();
-        vector<int> dp(n);
-        int result = INT_MIN;
-        for (int i = n - 1; i >= 0; --i) {
-            dp[i] = energy[i] + ((i + k < n) ? dp[i + k] : 0);
-            result = max(result, dp[i]);
+        n = energy.size();
+        K = k;
+        memo.resize(n, INT_MIN);
+        int ans = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, dfs(i, energy));
         }
-        return result;
+        return ans;
     }
 };
